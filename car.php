0ea5895f3182419ebf18b89690606a0cd86fc310
Florent-Vasseur <vasseurflorent@gmail.com>
2022-10-10 15:23:21 +0200

    quête ok

---
+++ car.php
@@ -0,0 +1,125 @@
+<?php 
+
+class Car
+{
+    public const TYPE_OF_ENERGY = ['diesel', 'gasoline', 'LPG', 'ethanol', 
+    'hydrogen', 'hybrid', 'electricity', 'other'];
+
+    private int $nbWheels=4;
+    private int $currentSpeed=0;
+    private string $color;
+    private int $nbSeats;
+    private string $energy;
+    private int $energyLevel=100;
+
+    /* CONSTRUCTEUR */
+
+    public function __construct(string $color='blue', int $nbSeats=4, string $energy='diesel')
+    {
+        $this->color = $color;
+        $this->setNbSeats($nbSeats);
+        $this->setEnergy($energy);
+    }
+
+    /* METHODS */
+
+    public function start() : string
+    {
+        $this->currentSpeed = 1;
+        return "Let's go slowly!!";
+    }
+    
+    public function forward(int $velocity=10) : string
+    {
+        if (($this->currentSpeed+$velocity)>130) {
+            $this->currentSpeed = 130;
+            return "Max Speed Reach !";
+        } else {
+            $this->currentSpeed += $velocity;
+            return "Increasing Speed !";
+        }
+    }
+
+    public function brake(): string
+    {
+        $sentence = "";
+        while ($this->currentSpeed > 0) {
+            $this->currentSpeed-=5;
+            $sentence .= "Brake !!!" . '<br>';
+        }
+        $sentence .= "I'm stopped !";
+        return $sentence;
+    }
+
+    /* GETTERS & SETTERS */
+
+    public function getNbSeats() : int
+    {
+        return $this->nbSeats;
+    }
+
+    public function setNbSeats(int $nbSeats) : void
+    {
+        $this->nbSeats = ($nbSeats<0) ? 4 : $nbSeats;
+    }
+
+    public function getNbWheels() : int
+    {
+        return $this->nbWheels;
+    }
+
+    public function setNbWheels(int $nbWheels) : void
+    {
+        $this->nbWheels = ($nbWheels<0) ? 4 : $nbWheels;
+    }
+
+    public function getColor() : string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): void
+    {
+        $this->color = $color;
+    }
+
+    public function getCurrentSpeed(): int
+    {
+        return $this->currentSpeed;
+    }
+
+    public function getEnergy() : string
+    {
+        return $this->energy;
+    }
+
+    public function setEnergy(string $energy) : void
+    {
+        $this->energy = in_array($energy, self::TYPE_OF_ENERGY) ? $energy : 'other';
+    }
+
+    public function getEnergyLevel() : int
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel) : void
+    {
+        if ($energyLevel > 100) {
+            $this->energyLevel = 100;
+        } elseif ($energyLevel < 0) {
+            echo 'ici';
+            $this->energyLevel = 0;
+        } else {
+            echo 'là';
+            $this->energyLevel = $energyLevel;
+        }
+    }
+
+}
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php 
+
+require './bicycle.php';
+require './car.php';
+
+/* code d'essai des méthodes */
+
+$bike = new Bicycle('blue');
+$bike->setCurrentSpeed(10);
+
+var_dump($bike);
+
+echo $bike->forward();
+echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
+echo $bike->brake();
+echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
+echo $bike->brake();
+$bike->dump();
+
+$car = new Car('blue', 4, 'diesel');
+var_dump($car);
+echo $car->setNbSeats(6) . '<br>';
+echo $car->getNbSeats() . '<br>';
+
+echo $car->setNbWheels(6) . '<br>';
+echo $car->getNbWheels() . '<br>';
+
+
+echo $car->setColor('red')  . '<br>';
+echo $car->getColor()  . '<br>';
+
+echo $car->getCurrentSpeed()  . '<br>';
+
+echo $car->setEnergy('electricity')  . '<br>';
+echo $car->getEnergy()  . '<br>';
+
+echo $car->setEnergyLevel(90)  . '<br>';
+echo $car->getEnergyLevel()  . '<br>';
+
+echo $car->start();
+var_dump($car);
+
+$car2 = new Car(nbSeats:-7, energy:'LPG');
+var_dump($car2);
+
+echo $car->forward();
+var_dump($car);
+
+echo $car->forward();
+var_dump($car);
+
+echo $car->forward(50);
+var_dump($car);
+
+echo $car->forward(50);
+var_dump($car);
+
+echo $car->forward(50);
+var_dump($car);
+
+echo $car->forward(50);
+var_dump($car);
+
+echo $car->brake();
+var_dump($car);
+
+echo $car->setEnergyLevel(50);
+var_dump($car);
+
+echo $car->setEnergyLevel(2110);
+var_dump($car);
+
+
+
+?>
